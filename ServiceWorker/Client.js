6f2ecb1bf20d61b84c6a2c7cb7d63bb0d58df818
GitHub-2021-07-12 <GitHub-2021-07-12@yandex.ru>
2021-08-31 19:36:14 +0300

    0.3.1

--- ServiceWorker/Client.js
+++ ServiceWorker/Client.js
@@ -1,1 +1,7 @@
 navigator.serviceWorker.register('./ServiceWorker.js');
+
+
+
+
+navigator.serviceWorker.addEventListener('controllerchange', () => alert('controllerchange'));
+navigator.serviceWorker.controller?.addEventListener('statechange', function () {alert('statechange ' + this.state)});
