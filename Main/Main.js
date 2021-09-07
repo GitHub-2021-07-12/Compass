0e9c9db5ce0ec9da3f8d8dc15990f437b47ff4a1
GitHub-2021-07-12 <GitHub-2021-07-12@yandex.ru>
2021-09-07 14:59:11 +0300

    0.5.15

--- Main/Main.js
+++ Main/Main.js
@@ -5,12 +5,18 @@ import '../Components/Compass/Compass.js';
 
 
 let _compass = document.querySelector('c-compass');
+let _input = document.querySelector('input');
 
 
 
 
 function _compass_on_pointerDown() {
   this.hasAttribute('animated') ? this.removeAttribute('animated') : this.setAttribute('animated', true);
+}
+
+
+function _input_on_change() {
+  _compass.style.setProperty('--animation_duration', _input.value);
 }
 
 
